444c1036013671d8c2af01ab902ab4e747e301af
GwynethSegura <gwyneth.segura@grupolozamora.com.pe>
2024-08-01 12:53:46 -0500

    feat: carrusel del equipo en la sección nosotros

---
+++ js/nosotros.js
@@ -0,0 +1,16 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+let carouselTeams = new Swiper(".swiper-teams", {
+  slidesPerView: 3,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
