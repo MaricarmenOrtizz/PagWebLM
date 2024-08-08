ae934b092662f965c626d0db0f882161dbbf1b0b
GwynethSegura <gwyneth.segura@grupolozamora.com.pe>
2024-08-08 12:39:02 -0500

    carousel benefits card animation

--- js/oportunidadLaboral.js
+++ js/oportunidadLaboral.js
@@ -1,5 +1,26 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
+const jobOportunitiesCard = document.getElementsByClassName(
+  "job-oportunities-card"
+);
+
+for (const card of jobOportunitiesCard) {
+  console.log(card);
+  const imgPost = card.getElementsByClassName("img-post");
+  const imgBenefits = card.getElementsByClassName("img-benefits");
+  console.log(imgPost);
+  imgPost[0].addEventListener("mouseover", () => {
+    if(imgBenefits[0].style.height !== '25rem' )
+      imgBenefits[0].style.animation = "showCard 1s linear forwards";
+  });
+
+  imgBenefits[0].addEventListener("mouseout", (e) => {
+    console.log(e.relatedTarget);
+    if(imgBenefits[0].style.height !== '0')
+      imgBenefits[0].style.animation = "hideCard 1s linear forwards";
+  });
+}
+
 /*---JOB OPORTUNITIES---*/
 
 let carouselHiring = new Swiper(".swiper-job-oportunities", {
@@ -9,15 +30,16 @@ let carouselHiring = new Swiper(".swiper-job-oportunities", {
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
+    pauseOnMouseEnter: true,
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
-  breakpoints:{
-    769:{
-      slidesPerView: 2
-    }
-  }
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+    },
+  },
 });
